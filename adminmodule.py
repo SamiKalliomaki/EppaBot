eb9bf96eed598c1912a1794aca301dff9cd514cc
Sami Kalliomäki <sami.kalliomaki@kapsi.fi>
2015-02-24 13:50:16 +0200

    Nick command that allows changing bot nickname

--- adminmodule.py
+++ adminmodule.py
@@ -1,7 +1,18 @@
-from botmodule import BotModule
+from botmodule import BotModule, CmdException
 from permissionmodule import permission_required
 
 class AdminModule(BotModule):
 	@permission_required('admin.quit')
 	def handle_cmd_quit(self, conn, **kwargs):
 		self.bot.close()
+
+	@permission_required('admin.nick')
+	def handle_cmd_nick(self, conn, **kwargs):
+		params = kwargs['params']
+
+		if len(params) != 1:
+			raise CmdException('.nick newnick')
+
+		self.bot.nick = params[0]
+		for conn in self.bot.connections:
+			conn.msg_nick(params[0])
