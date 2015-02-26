8fb0c527aad52e332b8e612f7d989913fbb706f4
Sami Kalliomäki <sami.kalliomaki@kapsi.fi>
2015-02-26 15:14:31 +0200

    TitleModule now only shows title for the same url once in five minutes

--- titlemodule.py
+++ titlemodule.py
@@ -1,23 +1,46 @@
 import re
 import socket
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from html.parser import HTMLParseError
 from http.client import HTTPException
 from urllib import request
 from botmodule import BotModule
 
+# Time before showing title for the same url
+repeat_time_limit = timedelta(minutes=5)
 
 class TitleModule(BotModule):
+	def __init__(self, bot):
+		super().__init__(bot)
+
+		self.last_seen = {}
+
 	def handle_privmsg(self, conn, sender, target, msg, respond):
 		super().handle_privmsg(conn, sender, target, msg, respond)
 
 		match = re.search(r'https?://[\w\=\-\.\_\~\:\/\?\#]*', msg)
 
-		if match != None:
-			try:
-				soup = BeautifulSoup(request.urlopen(match.group(0), None, 1))
-			except (request.URLError, HTMLParseError, HTTPException, socket.timeout, socket.error):
-				pass
-			else:
-				if soup.title != None:
-					conn.msg_privmsg(respond, 'Title: {}'.format(soup.title.string))
+		if match == None:
+			return
+
+		url = match.group(0)
+		identifier = respond + '@' + conn.network
+
+		if not identifier in self.last_seen:
+			self.last_seen[identifier] = {}
+
+		last_seen_this = self.last_seen[identifier].get(url, datetime.min)
+		if datetime.now() - last_seen_this < repeat_time_limit:
+			self.bot.log('Already seen this url, skipping')
+			return
+
+		self.last_seen[identifier][url] = datetime.now()
+
+		try:
+			soup = BeautifulSoup(request.urlopen(url, None, 1))
+		except (request.URLError, HTMLParseError, HTTPException, socket.timeout, socket.error) as e:
+			self.bot.log('Error fetching title: ' + str(e))
+		else:
+			if soup.title != None:
+				conn.msg_privmsg(respond, 'Title: {}'.format(soup.title.string))
